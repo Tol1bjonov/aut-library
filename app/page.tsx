f9724a911ef5313414971d378b725777f180fa43
Yusufjon Tolibjonov <yusufjontolibjonov3@gmail.com>
2025-04-21 11:12:18 +0500

    fix: deploying

--- app/page.tsx
+++ app/page.tsx
@@ -180,7 +180,6 @@ export default function Home() {
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2998.4217372631542!2d69.33892359999999!3d41.27792500000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae5f7e492a66c1%3A0x77427ccd87b4ed9!2sAjou%20University%20in%20Tashkent!5e0!3m2!1sru!2s!4v1744230467005!5m2!1sru!2s"
                   className="w-full h-full border-0"
-                  allowFullScreen=""
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
                 ></iframe>
